caf092e810f48716ffc7dd0b928d5827b0a54644
evodify <social7332@yahoo.com>
2016-11-11 16:39:55 +0100

    vcfTab_to_callsTab.py added

---
+++ vcfTab_to_callsTab.py
@@ -0,0 +1,115 @@
+#! /usr/bin/env python2
+
+'''
+This script converts the table produced with VariantsToTable (GATK) to one-character coded genotype table (calls format).
+
+#Example input:
+
+CHROM   POS REF sample1 sample2 sample3 sample4 sample5 sample6 sample7 sample8
+chr_1   1   A   T/A ./. ./. A/A ./. ./. ./. ./.
+chr_1   2   C   T/C T/C ./. C/C C/C ./. C/C ./.
+chr_1   3   C   C/GCC   C/C ./. C/C C/C C/C C/C C/C
+chr_1   4   T   T/T T/T ./. T/T T/T T/T T/T T/T
+chr_2   1   A   A/A A/A ./. A/A A/A A/A A/A A/A
+chr_2   2   C   C/C C/C ./. C/C C/C C/C C/C C/C
+chr_2   3   C   AT/AT   AT/AT   AT/AT   AT/AT   AT/AT   AT/AT   AT/AT   AT/AT
+chr_2   4   C   C/C T/T C/C C/C C/C C/C C/C C/C
+chr_2   5   T   T/T C/C T/T C/T T/T C/T T/T T/T
+chr_3   1   G   G/G ./. ./. G/G ./. ./. ./. ./.
+chr_3   2   C   G/C C/C ./. C/C C/C ./. C/C ./.
+chr_3   3   CTT CTT/CTT CTT/C   CTT/C   CTT/CTT CTT/CTT CTT/CTT CTT/CTT CTT/CTT
+chr_3   4   TA  T/T T/T ./. T/T T/T T/T T/T T/TA
+chr_3   5   G   */* G/* ./. G/G G/G G/G C/C G/G
+
+#Example output:
+
+CHROM   POS REF sample1 sample2 sample3 sample4 sample5 sample6 sample7 sample8
+chr_1   1   A   W   N   N   A   N   N   N   N
+chr_1   2   C   Y   Y   N   C   C   N   C   N
+chr_1   3   C   N   C   N   C   C   C   C   C
+chr_1   4   T   T   T   N   T   T   T   T   T
+chr_2   1   A   A   A   N   A   A   A   A   A
+chr_2   2   C   C   C   N   C   C   C   C   C
+chr_2   3   C   N   N   N   N   N   N   N   N
+chr_2   4   C   C   T   C   C   C   C   C   C
+chr_2   5   T   T   C   T   Y   T   Y   T   T
+chr_3   1   G   G   N   N   G   N   N   N   N
+chr_3   2   C   S   C   N   C   C   N   C   N
+chr_3   3   N   N   N   N   N   N   N   N   N
+chr_3   4   N   T   T   N   T   T   T   T   N
+chr_3   5   G   -   N   N   G   G   G   C   G
+
+#Symbols Meaning:
+
+R -> G and A
+Y -> T and C
+M -> A and C
+K -> G and T
+S -> G and C
+W -> A and T
+N -> missing or indels
+
+#command:
+
+$ python vcfTab_to_callsTab.py -i input.table -o output.tab
+
+#contact:
+Dmytro Kryvokhyzha dmytro.kryvokhyzha@evobio.eu
+
+'''
+
+
+############################# modules #############################
+
+import sys, argparse
+import calls # my custom module
+
+############################# options #############################
+
+class MyParser(argparse.ArgumentParser): 
+   def error(self, message):
+      sys.stderr.write('error: %s\n' % message)
+      self.print_help()
+      sys.exit(2)
+
+parser = MyParser()
+parser.add_argument('-i', '--input', help = 'name of the input file', type=str, required=True)
+parser.add_argument('-o', '--output', help = 'name of the output file', type=str, required=True)
+args = parser.parse_args()
+
+############################# program #############################
+
+counter = 0
+
+print('Opening the file...')
+
+with open(args.input) as datafile:
+  header_line = datafile.readline()
+
+  print('Creating the output...')
+
+  outfile = open(args.output, 'w')
+  outfile.write(header_line) 
+
+  print('Converting...')
+
+  for line in datafile:
+    words = line.split()
+    chr_pos = words[0:2]
+    GT = words[2:]
+
+    GTone = calls.twoToOne(GT)
+
+    chr_posP = '\t'.join(str(e) for e in chr_pos)
+    alleleline = '\t'.join(str(e) for e in GTone)
+    outfile.write("%s\t%s\n" % (chr_posP, alleleline))
+
+    # track progress
+    counter += 1
+    if counter % 1000000 == 0:
+      print str(counter), "lines processed"
+
+datafile.close()
+outfile.close()
+print('Done!')
+ 
